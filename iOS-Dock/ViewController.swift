ae9071fa9419f8008362b89615f72ff4f414f7fb
Mina Shoaib Rahman <shoaibsaikat@gmail.com>
2024-09-11 21:03:39 +0600

    Bound image view outlet

--- iOS-Dock/ViewController.swift
+++ iOS-Dock/ViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imageView.backgroundColor = .clear
     }
 }
 
