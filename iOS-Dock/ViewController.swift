cfd2185af641a9ebc168bf488ba95e41e4f93a77
Mina Shoaib Rahman <shoaibsaikat@gmail.com>
2024-09-17 16:05:42 +0600

    image drop into parent view added. but drop to image not working, need to fix

--- iOS-Dock/ViewController.swift
+++ iOS-Dock/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIDropInteractionDelegate {
     
     @IBOutlet weak var imageView: UIImageView!
     
@@ -15,6 +15,24 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         imageView.backgroundColor = .clear
+        imageView.contentMode = .scaleAspectFill
+        view.addInteraction(UIDropInteraction(delegate: self))
+    }
+    
+    func dropInteraction(_ interaction: UIDropInteraction, canHandle session: UIDropSession) -> Bool {
+        return session.canLoadObjects(ofClass: UIImage.self)
+    }
+    
+    func dropInteraction(_ interaction: UIDropInteraction, sessionDidUpdate session: UIDropSession) -> UIDropProposal {
+        return UIDropProposal(operation: .copy)
+    }
+    
+    func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession) {
+        session.loadObjects(ofClass: UIImage.self, completion: { images in
+            if let image = images.first as? UIImage {
+                self.imageView.image = image
+            }
+        })
     }
 }
 
