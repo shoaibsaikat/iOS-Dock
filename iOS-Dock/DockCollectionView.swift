7fd4e71ea35e8a7b15cb1f39046dee9be6330969
Mina Shoaib Rahman <shoaibsaikat@gmail.com>
2024-09-25 15:16:32 +0600

    cleanup

--- iOS-Dock/DockCollectionView.swift
+++ iOS-Dock/DockCollectionView.swift
@@ -9,15 +9,18 @@ import UIKit
 
 class DockCollectionView: UICollectionView {
     
+    private let dockCorner = 15.0
+    private let insetPadding = 30.0
+    
     override func draw(_ rect: CGRect) {
         let rect = CGRect(origin: CGPoint.zero, size: bounds.size)
         UIColor.systemBackground.setFill()
         UIRectFill(rect)
-        let roundedDock = UIBezierPath(roundedRect: bounds, cornerRadius: 15)
+        let roundedDock = UIBezierPath(roundedRect: bounds, cornerRadius: dockCorner)
         roundedDock.addClip()
         UIColor.brown.setFill()
         roundedDock.fill()
         
-        contentInset = UIEdgeInsets(top: 0, left: 30, bottom: 0, right: 30)
+        contentInset = UIEdgeInsets(top: 0, left: insetPadding, bottom: 0, right: insetPadding)
     }
 }
